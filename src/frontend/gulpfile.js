0687f11fc57c7fc0803ce46c58d67e43b24cf589
Svyatoslav <s.lukashkevych@gmail.com>
2017-01-13 15:19:31 +0200

    configure gulp to build project

--- src/frontend/gulpfile.js
+++ src/frontend/gulpfile.js
@@ -1,2 +1,19 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    concat = require('gulp-concat');
 
+var outputPath = '../main/resources/frontend';
+
+gulp.task('default', ['js', 'html']);
+
+gulp.task('html', function () {
+    return gulp.src('code/**/*.html')
+        .pipe(gulp.dest(outputPath + '/views'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('code/**/*.js')
+        .pipe(babel({presets: ['es2015']}))
+        .pipe(concat('all.min.js'))
+        .pipe(gulp.dest(outputPath + '/js'))
+});
--- src/main/scala/controllers/IndexController.scala
+++ src/main/scala/controllers/IndexController.scala
@@ -3,7 +3,7 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.PathMatchers
 
 trait IndexController {
-  private val EntryHtmlPath = "frontend/index.html"
+  private val EntryHtmlPath = "frontend/views/index.html"
 
   val indexRoute = path(PathMatchers.PathEnd) {
     get(getFromResource(EntryHtmlPath))
